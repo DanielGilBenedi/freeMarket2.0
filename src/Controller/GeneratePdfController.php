e1cee1dcba51820759526e935b8dcc3f4eddb789
daniel <breano33@gmail.com>
2021-05-23 15:14:01 +0200

    generate pdf

--- src/Controller/GeneratePdfController.php
+++ src/Controller/GeneratePdfController.php
@@ -5,8 +5,8 @@ namespace App\Controller;
 
 
 use App\Entity\OrderItem;
-use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
-use Knp\Snappy\Pdf;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,7 +15,7 @@ class GeneratePdfController extends AbstractController
 {
     /**
      * @Route("generar_pdf/{id}", name="generar_pdf", methods={"POST","GET"})
-     * @param Pdf $pdf
+
      */
     public function generarPdf(Pdf $pdf, Request $request){
         $idOrder = $request->get('id');
@@ -26,18 +26,29 @@ class GeneratePdfController extends AbstractController
         foreach ($order as $orTot){
             $total+= $orTot->getTotal();
         }
-        $html = $this->render("OrderPdf.html.twig", $data = [
+        $options = new Options();
+        $options->set('defaultFont', 'Roboto');
+
+
+        $dompdf = new Dompdf($options);
+
+        $data = array(
+            'headline' => 'my headline'
+        );
+        $html = $this->renderView("OrderPdf.html.twig", $data = [
             'order' => $order,
             'total' => $total
         ]);
 
-        $filename = "pedido$idOrder.pdf";
-        return new PdfResponse(
-            $pdf->getOutputFromHtml($html),
-            $filename,
-            $this->redirectToRoute('user_index')
-        );
+
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+        $dompdf->stream("order-$idOrder.pdf", [
+            "Attachment" => true
+        ]);
 
 
     }
+
 }
