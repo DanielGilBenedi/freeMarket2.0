9d9d218ef2a051fe27868aa7a4487a3db51c6b3b
daniel <breano33@gmail.com>
2021-05-26 21:11:32 +0200

    facturas

--- src/Controller/GeneratePdfController.php
+++ src/Controller/GeneratePdfController.php
@@ -30,7 +30,7 @@ class GeneratePdfController extends AbstractController
             ->getRepository(Order::class)
             ->findBy(['id'=> $idOrder]);
 
-        dump($or[0][3]);
+        dump($or[0]->getIdCliente());
 
         $user = $this->getDoctrine()
             ->getRepository(User::class)
