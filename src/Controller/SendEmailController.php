df446ff112fc47728efc3f538c527e2b91b1dedd
daniel <breano33@gmail.com>
2021-05-29 12:49:56 +0200

    mailer

--- src/Controller/SendEmailController.php
+++ src/Controller/SendEmailController.php
@@ -5,8 +5,8 @@ namespace App\Controller;
 
 
 use App\Entity\User;
+use App\Form\EmailType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
