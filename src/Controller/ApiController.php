fd34e312e06b8c2928c8599fffff949819ff43d4
daniel <breano33@gmail.com>
2021-05-30 22:30:20 +0200

    api + busc

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -10,6 +10,7 @@ use App\Repository\ProductosRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/api")
